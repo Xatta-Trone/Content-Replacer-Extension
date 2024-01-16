7eb9e5eb8ad583e3f7684a2f1ea1a5a7c1973076
Monzurul Islam <monzurul.ce.buet@gmail.com>
2024-01-16 14:49:10 +0900

    execute script

--- src/background.js
+++ src/background.js
@@ -5,17 +5,51 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.type === 'GREETINGS') {
+//     const message = `Hi ${
+//       sender.tab ? 'Con' : 'Pop'
+//     }, my name is Bac. I am from Background. It's great to hear from you.`;
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
+//     // Log message coming from the `request` parameter
+//     console.log(request.payload.message);
+//     // Send a response message
+//     sendResponse({
+//       message,
+//     });
+//   }
+// });
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.sendMessage(tab.id, {
+    message: 'TOGGLE_MENU',
+  });
+});
+
+function getTitle(cb) {
+  return cb();
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'EXECUTE') {
+    console.log('execute');
+    let code = request.code;
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      chrome.scripting
+        .executeScript({
+          target: { tabId: activeTab.id },
+          func: (code) => {
+            const el = document.createElement('script');
+            el.textContent = code;
+            document.documentElement.appendChild(el);
+            el.remove();
+          },
+          args: [code],
+          world: 'MAIN',
+        })
+        .then(() => console.log('injected a function'));
     });
   }
+  return true;
 });
--- src/contentScript.js
+++ src/contentScript.js
@@ -11,33 +11,119 @@
 // For more information on Content Scripts,
 // See https://developer.chrome.com/extensions/content_scripts
 
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message == 'TOGGLE_MENU') {
+    console.log('TOGGLING MENU');
+    toggleBtnVisibility();
   }
-);
-
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
 });
+
+const IDS = {
+  divId: 'extension-capture-div',
+  button1Id: 'extension-capture-button',
+  button2Id: 'extension-capture-button2',
+  button22Id: 'extension-capture-button22',
+  button3Id: 'extension-close-button',
+};
+
+function createDiv() {
+  const div = document.createElement('div');
+  div.id = IDS.divId;
+  div.setAttribute(
+    'style',
+    'display:block; margin-top:10px;text-align:center;position:sticky;bottom:20px;left:100%;transform:translateX(-10%);z-index:99999;max-width:10%;'
+  );
+
+  const button = document.createElement('button');
+  button.id = IDS.button1Id;
+  button.innerText = 'Turn Grayscale';
+  button.setAttribute(
+    'style',
+    'padding:14px 25px; color:white; background: #242424;margin-bottom: 10px;cursor: pointer;'
+  );
+  button.onclick = () => turnGrayScale();
+
+  const button2 = document.createElement('button');
+  button2.id = IDS.button2Id;
+  button2.innerText = 'Record video';
+  button2.setAttribute(
+    'style',
+    'padding:14px 25px; color:white; background: #242424;margin-bottom: 10px;cursor: pointer;'
+  );
+
+  const button22 = document.createElement('button');
+  button22.id = IDS.button22Id;
+  button22.innerText = 'Inject JavaScript';
+  button22.setAttribute(
+    'style',
+    'padding:14px 25px; color:white; background: #242424;margin-bottom: 10px;cursor: pointer;'
+  );
+  button22.onclick = () => injectJS();
+
+  const button3 = document.createElement('button');
+  button3.innerHTML = '&times;';
+  button3.id = IDS.button3Id;
+  button3.setAttribute(
+    'style',
+    'font-size: 27px;position: absolute;top: 0;right: 0;border: 2px solid #242424;cursor: pointer; text-align: center;padding: 3px 8px; margin:0;color:white; background: #242424;'
+  );
+  button3.onclick = () => toggleBtnVisibility();
+
+  div.appendChild(button);
+  div.appendChild(button2);
+  div.appendChild(button22);
+  div.appendChild(button3);
+
+  return div;
+}
+
+function createBtnEl(text, id, onclick = () => {}) {
+  const button = document.createElement('button');
+  button.innerHTML = text;
+  button.id = id;
+  button.setAttribute(
+    'style',
+    'font-size: 27px;position: absolute;top: 0;right: 0;border: 2px solid #242424;cursor: pointer; text-align: center;padding: 3px 8px; margin:0;color:white; background: #242424;'
+  );
+  button.onclick = onclick;
+  return button;
+}
+
+function toggleBtnVisibility() {
+  let el = document.getElementById(IDS.divId);
+
+  if (el == null) {
+    const div = createDiv();
+    document.body.appendChild(div);
+  } else {
+    // Todo: remove all listeners
+    el.remove();
+  }
+}
+
+function turnGrayScale() {
+  // apply grayscale
+  let html = document.getElementsByTagName('html')[0];
+  html.setAttribute(
+    'style',
+    `-moz-filter: grayscale(100%);-webkit-filter: grayscale(100%);filter: gray;filter: grayscale(100%);`
+  );
+}
+
+function injectJS() {
+  const jsCode = prompt('Paste the Javascript code you want to execute here:');
+
+  if (jsCode == null || jsCode == '') {
+    return;
+  }
+
+  chrome.runtime.sendMessage(
+    {
+      type: 'EXECUTE',
+      code: jsCode,
+    },
+    (response) => {
+      console.log(response);
+    }
+  );
+}
