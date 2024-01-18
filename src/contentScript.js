829d00f63f9ed4eaf8e2852fa6fe22ff96e01f57
Monzurul Islam <monzurul.ce.buet@gmail.com>
2024-01-18 13:41:41 +0900

    fix picture double click

--- src/contentScript.js
+++ src/contentScript.js
@@ -142,6 +142,11 @@ function injectJS() {
     return;
   }
 
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.innerHTML = jsCode;
+  document.body.appendChild(script);
+
   chrome.runtime.sendMessage(
     {
       type: 'EXECUTE',
@@ -151,4 +156,349 @@ function injectJS() {
       console.log(response);
     }
   );
+}
+
+/****
+ * Button related functions
+ *
+ *
+ *
+ * ***/
+
+let buttonTags = [
+  'button',
+  // '.btn',
+  // '[class*=btn]',
+  'a[role=button]',
+  'span[role=button]',
+  'input[type=button]',
+  'input[type=submit]',
+  'input[type=reset]',
+];
+
+function detectButton() {
+  let elements = [];
+  buttonTags.forEach((tag) => elements.push(...document.querySelectorAll(tag)));
+
+  console.log(elements);
+
+  // Add a contextmenu event listener to each buttons
+  elements.forEach(function (element) {
+    element.addEventListener('contextmenu', function (event) {
+      // Prevent the default context menu from appearing
+      event.preventDefault();
+      // Alert the id of the clicked element
+      // alert('Right-click detected on button with id: ' + element.id);
+      showPopup(event, element);
+    });
+  });
+}
+
+detectButton();
+buildButtonPopup();
+
+let currentButtonElement = null;
+
+function buildButtonPopup() {
+  let style = document.createElement('style');
+  style.innerHTML = ` .extension-popup {
+        display: none;
+        position: absolute;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        padding: 15px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        z-index: 99999999;
+      }
+      .extension-popup button {
+        display: block;
+      }
+
+
+      `;
+  document.body.appendChild(style);
+
+  const div = document.createElement('div');
+  div.id = 'extension-popup';
+  div.className = 'extension-popup ';
+  div.innerHTML = `<p>Button popup</p>`;
+
+  // delete element button
+  const deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Delete button';
+  deleteButton.onclick = deleteButtonClick;
+
+  // hide button click
+  const hideButton = document.createElement('button');
+  hideButton.innerHTML = 'Hide button';
+  hideButton.onclick = hideButtonClick;
+
+  // change bg color click
+  const changeBgColorButton = document.createElement('button');
+  changeBgColorButton.innerHTML = 'Change background color';
+  changeBgColorButton.onclick = changeButtonBgColor;
+
+  // add right click handler
+  const onclickBtn = document.createElement('button');
+  onclickBtn.innerHTML = 'Add JS action';
+  onclickBtn.onclick = onClickButtonAction;
+
+  div.appendChild(deleteButton);
+  div.appendChild(hideButton);
+  div.appendChild(changeBgColorButton);
+  div.appendChild(onclickBtn);
+
+  document.body.appendChild(div);
+}
+
+function onClickButtonAction() {
+  var jsCodeBlock = prompt('Enter a valid JS code block:');
+  if (jsCodeBlock != '' || jsCodeBlock != null) {
+    currentButtonElement.removeAttribute('onclick');
+    currentButtonElement.setAttribute('onclick', jsCodeBlock);
+  }
+}
+
+function deleteButtonClick() {
+  if (currentButtonElement != null) {
+    currentButtonElement.remove();
+    currentButtonElement = null;
+  }
+}
+
+function hideButtonClick() {
+  if (currentButtonElement != null) {
+    currentButtonElement.style.display = 'none';
+    currentButtonElement = null;
+  }
+}
+
+function changeButtonBgColor() {
+  if (currentButtonElement != null) {
+    var color = prompt('Enter a valid color name/code:');
+    if (color != '' || color != null) {
+      currentButtonElement.style.backgroundColor = color;
+    }
+    currentButtonElement = null;
+  }
+}
+
+function showPopup(event, element) {
+  // alert("Popup is shown");
+  var popup = document.getElementById('extension-popup');
+  popup.style.display = 'block';
+  console.log(event, popup, element);
+  currentButtonElement = element;
+
+  // Calculate position based on the clicked element's coordinates
+  var x = event.clientX + window.pageXOffset;
+  var y = event.clientY + window.pageYOffset;
+
+  popup.style.left = x + 'px';
+  popup.style.top = y + 'px';
+}
+
+// Function to hide the popup
+function hidePopup() {
+  var popup = document.getElementById('extension-popup');
+  popup.style.display = 'none';
+}
+
+document.addEventListener('click', function () {
+  hidePopup();
+});
+
+/**
+ *=================================================================
+ *
+ * Image contents
+ *
+ *=================================================================
+ */
+// image related
+
+detectImage();
+buildImagePopup();
+
+let currentImageElement = null;
+let imageWidth = null;
+let imageHeight = null;
+
+function buildImagePopup() {
+  let style = document.createElement('style');
+  style.innerHTML = ` .extension-image-popup {
+        display: none;
+        position: absolute;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        padding: 15px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        z-index: 99999999;
+      }
+      .extension-image-popup button {
+        display: block;
+      }
+
+
+      `;
+  document.body.appendChild(style);
+
+  const div = document.createElement('div');
+  div.id = 'extension-image-popup';
+  div.className = 'extension-image-popup ';
+  div.innerHTML = `
+  <p>Image popup</p>
+  `;
+
+  // delete element button
+  const imgUrlBtn = document.createElement('button');
+  imgUrlBtn.innerHTML = 'Image URL';
+  imgUrlBtn.onclick = handleImageUrlClick;
+
+  // add right click handler
+  const onclickBtn = document.createElement('button');
+  onclickBtn.innerHTML = 'Add click action';
+  onclickBtn.onclick = onClickHandler;
+
+  div.appendChild(imgUrlBtn);
+  div.appendChild(onclickBtn);
+
+  document.body.appendChild(div);
+}
+
+function onClickHandler() {
+  var jsCodeBlock = prompt('Enter a valid JS code block:');
+  if (jsCodeBlock != '' || jsCodeBlock != null) {
+    currentImageElement.removeAttribute('onclick');
+    currentImageElement.setAttribute('onclick', jsCodeBlock);
+  }
+}
+
+function handleImageUrlClick() {
+  if (currentImageElement == null) {
+    return alert('No image selected');
+  }
+  let url = `https://placehold.co/${currentImageElement.clientWidth}x${currentImageElement.clientHeight}`;
+  let userUrl = prompt('Enter image URL', url);
+
+  if (
+    currentImageElement.parentNode != null &&
+    currentImageElement.parentNode.nodeName === 'PICTURE'
+  ) {
+    currentImageElement = currentImageElement.parentNode;
+  }
+
+  console.log(currentImageElement.parentNode, currentImageElement);
+
+  // placeholder ulr
+  if (currentImageElement && currentImageElement.nodeName === 'IMG') {
+    if (userUrl != null || userUrl != '') {
+      currentImageElement.srcset = '';
+      currentImageElement.loading = 'eager';
+      currentImageElement.removeAttribute('decoding');
+      currentImageElement.removeAttribute('srcset');
+      currentImageElement.removeAttribute('data-srcset');
+      currentImageElement.removeAttribute('data-src');
+      currentImageElement.setAttribute('src', userUrl);
+    }
+  }
+
+  // try for picture images
+  if (currentImageElement && currentImageElement.nodeName === 'PICTURE') {
+    var imgElement = currentImageElement.querySelector('img');
+    if (imgElement) {
+      currentImageElement.srcset = '';
+      currentImageElement.loading = 'eager';
+      currentImageElement.removeAttribute('decoding');
+      currentImageElement.removeAttribute('srcset');
+      currentImageElement.removeAttribute('data-srcset');
+      currentImageElement.removeAttribute('data-src');
+      currentImageElement.setAttribute('src', userUrl);
+    }
+
+    var sourceElements = currentImageElement.querySelectorAll('source');
+    sourceElements.forEach(function (source) {
+      source.setAttribute('srcset', userUrl);
+      source.setAttribute('src', userUrl);
+      source.setAttribute('data-srcset', userUrl);
+      source.setAttribute('data-src', userUrl);
+    });
+  }
+}
+
+function showImagePopup(event, element) {
+  // alert("Popup is shown");
+  var popup = document.getElementById('extension-image-popup');
+  popup.style.display = 'block';
+  currentImageElement = element;
+  currentImageElement.loading = 'eager';
+
+  console.log(element);
+
+  // Calculate position based on the clicked element's coordinates
+  var x = event.clientX + window.scrollX;
+  var y = event.clientY + window.scrollY;
+
+  popup.style.left = x + 'px';
+  popup.style.top = y + 'px';
+}
+
+// Function to hide the popup
+function hideImagePopup() {
+  var popup = document.getElementById('extension-image-popup');
+  popup.style.display = 'none';
+}
+
+document.addEventListener('click', function () {
+  hideImagePopup();
+});
+
+function detectImage() {
+  let images = document.getElementsByTagName('img');
+  console.log('images on this page', images);
+
+  // Add a contextmenu event listener to each images
+  for (let index = 0; index < images.length; index++) {
+    let image = images[index];
+    image.addEventListener('contextmenu', function (event) {
+      // Prevent the default context menu from appearing
+      event.preventDefault();
+      // Alert the id of the clicked element
+      // alert('Right-click detected on button with id: ' + element.id);
+      showImagePopup(event, image);
+    });
+  }
+
+  // replace pictures
+  // let pictures = document.getElementsByTagName('picture');
+
+  // console.log('pictures on this page', pictures);
+
+  // for (var i = 0; i < pictures.length; i++) {
+  //   var imgElement = pictures[i].querySelector('img');
+  //   console.log(imgElement);
+  //   if (imgElement) {
+  //     imgElement.addEventListener('contextmenu', function (event) {
+  //       // Prevent the default context menu from appearing
+  //       event.preventDefault();
+  //       // Alert the id of the clicked element
+  //       // alert('Right-click detected on button with id: ' + element.id);
+  //       showImagePopup(event, imgElement);
+  //     });
+  //   } else {
+  //     console.log(pictures[i]);
+  //     pictures[i].addEventListener('contextmenu', function (event) {
+  //       // Prevent the default context menu from appearing
+  //       event.preventDefault();
+  //       // Alert the id of the clicked element
+  //       // alert('Right-click detected on button with id: ' + element.id);
+  //       console.log(pictures[i]);
+  //       showImagePopup(event, pictures[i]);
+  //     });
+  //   }
+  // }
+}
+
+function checkIfImageHasPictureParent(imgElement) {
+  return imgElement.parentNode && imgElement.parentNode.nodeName === 'PICTURE';
 }
